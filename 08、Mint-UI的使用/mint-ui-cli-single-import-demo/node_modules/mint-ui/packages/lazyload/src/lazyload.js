6e613882a5417fbdb5da3706bfd9be06d3f4eadf
TigerChain <git_dev@163.com>
2018-05-09 21:54:27 +0800

    删除node_modules文件夹

--- 08、Mint-UI的使用/mint-ui-cli-single-import-demo/node_modules/mint-ui/packages/lazyload/src/lazyload.js
+++
@@ -1,4 +0,0 @@
-import Lazyload from 'vue-lazyload';
-import 'mint-ui/src/style/empty.css';
-
-export default Lazyload;
--- 08、Mint-UI的使用/mint-ui-cli-single-import-demo/node_modules/mint-ui/packages/tab-container/_index.js
+++
@@ -1,4 +0,0 @@
-export default {
-  TabContainer: require('./src/tab-container.vue'),
-  TabContainerItem: require('../tab-container-item/src/tab-container-item.vue')
-};
